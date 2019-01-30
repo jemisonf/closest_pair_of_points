21a7cd3bdaa44361cbdc7c27e11f814768d763ca
Fischer Jemison <jemisonf@oregonstate.edu>
2019-01-30 12:09:00 -0800

    adds lib.py

---
+++ lib.py
@@ -0,0 +1,8 @@
+# read an input file and return a list of tuples where each tuple is an (x, y) pair
+def read_inputs(filename):
+    return 0
+
+# input is the filename, the distance as a float, and a list of tuples of tuples, 
+#       where each tuple of tuples is a pair of (x, y) points such as ((1, 0), (0, 1))
+def write_outputs(filename, distance, points):
+    return 0
