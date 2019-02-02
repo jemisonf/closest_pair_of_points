d5ab98ecb59716df97e51a874d91cf3f1fae547c
ianbrown9475 <ianbrown9475@gmail.com>
2019-02-02 13:33:24 -0800

    WIP: Finish crude implementation of enhanced dnd

--- enhanceddnc.py
+++ enhanceddnc.py
@@ -1,5 +1,6 @@
 from math import pow, sqrt
 from statistics import median
+from sys import argv
 from typing import List, Tuple
 
 import lib
@@ -10,25 +11,28 @@ test_data_2 = [(1, 2), (3, 4)]
 test_data_3 = [(1, 2), (3, 4), (5, 6)]
 
 
-def closest_pair(points: List[Tuple]) -> List[Tuple[Tuple]]:
+def closest_pair(points: List[Tuple]) -> Tuple[float, List[Tuple[Tuple]]]:
     if len(points) == 2:
         result = list()
         result.append(tuple(points))
-        print(f"returning {result}")
-        return result
+        final_result = (compute_distance(points[0], points[1]), result)
+        print(f"returning {final_result}")
+        return final_result
     if len(points) == 3:
         distances = {
             (points[0], points[1]): compute_distance(points[0], points[1]),
             (points[1], points[2]): compute_distance(points[1], points[2]),
             (points[0], points[2]): compute_distance(points[0], points[2])
         }
-        print(f"min distance: {min(distances.values())}")
+        min_distance = min(distances.values())
+        print(f"min distance: {min_distance}")
         result = list(filter(
-            lambda it: distances[it] == min(distances.values()),
+            lambda it: distances[it] == min_distance,
             distances.keys()
         ))
-        print(f"returning {result}")
-        return result
+        final_result = (min_distance, result)
+        print(f"returning {final_result}")
+        return final_result
 
     median_x = median(map(lambda it: it[0], points))
     print(f"computed median_x: {median_x}")
@@ -36,12 +40,12 @@ def closest_pair(points: List[Tuple]) -> List[Tuple[Tuple]]:
     right_half = list(filter(lambda it: it[0] > median_x, points))
     print(f"left_half: {left_half}")
     print(f"right_half: {right_half}")
+    print("\nrecursive call on left")
     closest_left = closest_pair(left_half)
+    print("\nrecursive call on right")
     closest_right = closest_pair(right_half)
-    delta = min([
-        compute_distance(closest_left[0][0], closest_left[0][1]),
-        compute_distance(closest_right[0][0], closest_right[0][1])
-    ])
+    print()
+    delta = min([closest_left[0], closest_right[0]])
     print(f"computed delta: {delta}")
     middle_band = list(filter(
         lambda it: median_x - delta <= it[0] <= median_x + delta,
@@ -49,21 +53,41 @@ def closest_pair(points: List[Tuple]) -> List[Tuple[Tuple]]:
     ))
     print(f"middle_band: {middle_band}")
     # Sort by y coord
+    middle_band.sort(key=lambda it: it[1])
     # Call closest cross pair
+    closest_cross = closest_cross_pair(middle_band, delta)
+    print(f"closest_cross: {closest_cross}")
+    new_min = min(delta, closest_cross[0])
+    print(f"new_min: {new_min}")
+    result = (new_min, [])
+    for section in [closest_left, closest_right, middle_band]:
+        if section[0] == new_min:
+            result[1].extend(section[1])
+    print(f"result: {result}")
+    return result
 
 
 def closest_cross_pair(
     points: List[Tuple], delta: float
-) -> List[Tuple[Tuple]]:
+) -> Tuple[float, List[Tuple[Tuple]]]:
     dm = delta
     for i in range(len(points) - 1):
         j = i + 1
-        while(points[j][1] - points[i][1] <= delta and j <= len(points)):
+        while(j < len(points) and points[j][1] - points[i][1] <= delta):
             d = compute_distance(points[0], points[1])
             dm = min(d, dm)
             j = j + 1
     print(f"min distance in cross pairs: {dm}")
-    #TODO return closest pairs without increasing runtime
+    pairs = list((x, y) for x in points for y in points)
+    print(f"pairs: {pairs}")
+    # for pair in pairs:
+    #     print(compute_distance(pair[0], pair[1]))
+    result = list(filter(
+        lambda it: compute_distance(it[0], it[1]) <= dm and it[0] != it[1],
+        pairs
+    ))
+    print(f"result: {result}")
+    return (dm, result)
 
 
 def compute_distance(point_a, point_b):
@@ -73,6 +97,9 @@ def compute_distance(point_a, point_b):
 
 
 if __name__ == "__main__":
-    inputs = test_data
+    try:
+        inputs = lib.read_inputs(argv[1])
+    except IndexError:
+        exit("No input file was provided")
     print(f"inputs: {inputs}")
     print(f"closest pair: {closest_pair(inputs)}")
