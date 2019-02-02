1e2586a974281ef00a7e51d165ea2f68c883a128
ianbrown9475 <ianbrown9475@gmail.com>
2019-02-02 15:05:23 -0800

    Remove type annotations, add comments

--- enhanceddnc.py
+++ enhanceddnc.py
@@ -1,12 +1,14 @@
 from math import pow, sqrt
 from statistics import median
 from sys import argv
-from typing import List, Tuple
 
 from lib import read_inputs, write_outputs
 
 
-def closest_pair(points: List[Tuple]) -> Tuple[float, List[Tuple[Tuple]]]:
+# Computes the closest pairs and the distance between these two points
+# Returns a tuple of a float and a list of tuples of tuples representing pairs
+# of points
+def closest_pair(points):
     if len(points) == 2:
         result = list()
         result.append(tuple(points))
@@ -50,9 +52,11 @@ def closest_pair(points: List[Tuple]) -> Tuple[float, List[Tuple[Tuple]]]:
     return result
 
 
-def closest_cross_pair(
-    points: List[Tuple], delta: float
-) -> Tuple[float, List[Tuple[Tuple]]]:
+# Compute the closest cross-pair
+# points should be sorted by y-coordinate in ascending order
+# Returns a tuple with first item being the shortest distance and the second
+# item being the list of tuples with distance equal to the shortest distance
+def closest_cross_pair(points, delta):
     dm = delta
     for i in range(len(points) - 1):
         j = i + 1
@@ -60,15 +64,16 @@ def closest_cross_pair(
             d = compute_distance(points[0], points[1])
             dm = min(d, dm)
             j = j + 1
-    pairs = list((x, y) for x in points for y in points)
-    result = list(filter(
+    possible_pairs = list((x, y) for x in points for y in points)
+    closest_pairs = list(filter(
         lambda it: compute_distance(it[0], it[1]) <= dm and it[0] != it[1],
-        pairs
+        possible_pairs
     ))
-    final_result = (dm, result)
+    final_result = (dm, closest_pairs)
     return final_result
 
 
+# Compute the distance between two points
 def compute_distance(point_a, point_b):
     return sqrt(
         pow(point_b[0] - point_a[0], 2) + pow(point_b[1] - point_a[1], 2)
