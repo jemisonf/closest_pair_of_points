bfd768f344489aeb92fe16dd803f41d8bc813636
ianbrown9475 <ianbrown9475@gmail.com>
2019-02-02 15:13:41 -0800

    Refactor, reformat, improve documentation

--- enhanceddnc.py
+++ enhanceddnc.py
@@ -9,11 +9,51 @@ from lib import read_inputs, write_outputs
 # Returns a tuple of a float and a list of tuples of tuples representing pairs
 # of points
 def closest_pair(points):
+    # Manually compute closest pair if length is 2 or 3
+    if len(points <= 3):
+        return simple_closest_pair(points)
+
+    # Compute the median x value
+    median_x = median(map(lambda it: it[0], points))
+
+    # Split into left and right portions
+    left_half = list(filter(lambda it: it[0] <= median_x, points))
+    right_half = list(filter(lambda it: it[0] > median_x, points))
+
+    # Recursively call closest_pair on both halves
+    closest_left = closest_pair(left_half)
+    closest_right = closest_pair(right_half)
+
+    # Find the minimum distance delta between both sides and use to make middle
+    # band
+    delta = min([closest_left[0], closest_right[0]])
+    middle_band = list(filter(
+        lambda it: median_x - delta <= it[0] <= median_x + delta,
+        points
+    ))
+
+    # Sort by y coord and get closest cross pair
+    middle_band.sort(key=lambda it: it[1])
+    closest_cross = closest_cross_pair(middle_band, delta)
+
+    # Update minimum distance if smaller distance was found in cross pair
+    new_min = min(delta, closest_cross[0])
+    result = (new_min, [])
+    for section in [closest_left, closest_right, closest_cross]:
+        if section[0] == new_min:
+            for pair in section[1]:
+                if not any(x in result[1] for x in [pair, (pair[1], pair[0])]):
+                    result[1].append(pair)
+    return result
+
+
+# Compute closest pair for length 2 or 3
+def simple_closest_pair(points):
     if len(points) == 2:
         result = list()
         result.append(tuple(points))
-        final_result = (compute_distance(points[0], points[1]), result)
-        return final_result
+        return (compute_distance(points[0], points[1]), result)
+
     if len(points) == 3:
         distances = {
             (points[0], points[1]): compute_distance(points[0], points[1]),
@@ -25,31 +65,7 @@ def closest_pair(points):
             lambda it: distances[it] == min_distance,
             distances.keys()
         ))
-        final_result = (min_distance, result)
-        return final_result
-
-    median_x = median(map(lambda it: it[0], points))
-    left_half = list(filter(lambda it: it[0] <= median_x, points))
-    right_half = list(filter(lambda it: it[0] > median_x, points))
-    closest_left = closest_pair(left_half)
-    closest_right = closest_pair(right_half)
-    delta = min([closest_left[0], closest_right[0]])
-    middle_band = list(filter(
-        lambda it: median_x - delta <= it[0] <= median_x + delta,
-        points
-    ))
-    # Sort by y coord
-    middle_band.sort(key=lambda it: it[1])
-    # Call closest cross pair
-    closest_cross = closest_cross_pair(middle_band, delta)
-    new_min = min(delta, closest_cross[0])
-    result = (new_min, [])
-    for section in [closest_left, closest_right, closest_cross]:
-        if section[0] == new_min:
-            for pair in section[1]:
-                if not any(x in result[1] for x in [pair, (pair[1], pair[0])]):
-                    result[1].append(pair)
-    return result
+        return (min_distance, result)
 
 
 # Compute the closest cross-pair
